091446e0d013e60363e4277cc83b7729f38c469a
dylan-dev <d1l4ndev21@gmail.com>
2026-07-21 14:11:00 -0500

    Creacion del esquema que usara la base de datos.

---
+++ plan-view/src/db/schema.ts
@@ -0,0 +1,29 @@
+/* Esta es la entidad del plano */
+export interface Drawing { 
+  id: string;
+  name: string;
+  imagePath: string;
+  width: number;
+  height: number;
+  createdAt: number;
+}
+/* Esta es la entidad del objeto en el plano */
+export interface DrawingObject {
+  id: string;
+  drawingId: string;
+  code: string;
+  description?: string;
+  x: number;
+  y: number;
+  createdAt: number;
+}
+/* Esta es la entidad de la foto del objeto en el plano */
+export interface ObjectPhoto {
+  id: string;
+  objectId: string;
+  blob: Blob;
+  fileName: string;
+  mimeType: string;
+  size: number;
+  createdAt: number;
+}
