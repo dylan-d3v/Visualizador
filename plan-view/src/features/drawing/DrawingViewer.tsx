62415da7d7ba2bcaba2038804819d6a0bfc4f9bd
dylan-dev <d1l4ndev21@gmail.com>
2026-07-21 20:22:47 -0500

    Componente vista del plano con los objetos del mismo.

---
+++ plan-view/src/features/drawing/DrawingViewer.tsx
@@ -0,0 +1,30 @@
+// Llamando a los datos de los objetos de dibujo desde el archivo drawingObjects.ts
+import { drawingObjects } from "../../data/drawingObjects";
+// Este componente muestra la vista del plano con los objetos de dibujo superpuestos en sus posiciones correspondientes
+export function DrawingViewer() {
+  return (
+    <div className="relative w-full overflow-hidden">
+      <img
+        src="/drawings/plano-1.webp"
+        alt="General Arrangement"
+        className="block w-full"
+      />
+
+      {drawingObjects.map((object) => ( /* Muestra cada objeto de dibujo en su posición correspondiente */
+        <button
+          key={object.id}
+          type="button"
+          className="absolute h-4 w-4 -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-600"
+          style={{
+            left: `${object.x * 100}%`,
+            top: `${object.y * 100}%`,
+          }}
+        >
+          <span className="sr-only">
+            {object.code}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
+}
