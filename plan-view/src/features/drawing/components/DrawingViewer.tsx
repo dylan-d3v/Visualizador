8a9f9485b4ba416f6cd70fd1e16b4874b84cf408
dylan-dev <d1l4ndev21@gmail.com>
2026-07-21 22:01:14 -0500

    Actualizando DrawingViewer.tsx y adjuntando el componente marcador de los objetos del plano.

--- plan-view/src/features/drawing/components/DrawingViewer.tsx
+++ plan-view/src/features/drawing/components/DrawingViewer.tsx
@@ -1,29 +1,23 @@
 // Llamando a los datos de los objetos de dibujo desde el archivo drawingObjects.ts
 import { drawingObjects } from "../../../data/drawingObjects";
+// Importando el componente DrawingObject que representa cada objeto de dibujo en la vista del plano
+import  { DrawingObject } from "./DrawingObject";
 // Este componente muestra la vista del plano con los objetos de dibujo superpuestos en sus posiciones correspondientes
 export function DrawingViewer() {
   return (
     <div className="relative w-full overflow-hidden">
       <img
         src="/drawings/plano-1.webp"
-        alt="General Arrangement"
+        alt="Plano"
         className="block w-full"
       />
 
       {drawingObjects.map((object) => ( /* Muestra cada objeto de dibujo en su posición correspondiente */
-        <button
-          key={object.id}
-          type="button"
-          className="object-marker"
-          style={{
-            left: `${object.x * 100}%`,
-            top: `${object.y * 100}%`,
-          }}
-        >
-          <span className="sr-only">
-            {object.code}
-          </span>
-        </button>
+        <DrawingObject 
+        key={object.id} 
+        object={object} 
+        />
+
       ))}
     </div>
   );
